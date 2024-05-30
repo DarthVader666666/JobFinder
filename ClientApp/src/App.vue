0c9c35f50e44e5a5c1c787788cdf205c439a0568
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-05-30 12:48:10 +0300

    Added client app

---
+++ ClientApp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="findJobs()">Find Job</button>
+  <p>{{ jobs }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: ''
+    }
+  },
+
+  methods: {
+    findJobs() {
+      var result = fetch(
+        'https://rabota.by/vacancies/net-developer',
+        //'https://www.google.com/search?q=.net+%D0%B2%D0%B0%D0%BA%D0%B0%D0%BD%D1%81%D0%B8%D0%B8',
+        {
+          mode: 'no-cors',
+          headers: {
+            'Access-Control-Allow-Origin':'*'
+          }
+        }
+      )
+      .then(result => {
+        console.log(result);
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
