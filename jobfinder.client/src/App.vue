33f55e55fade9358f3a0820df9742ec977ac08a0
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-06-21 21:28:53 +0300

    Rebuilt project as single fullstack app

--- jobfinder.client/src/App.vue
+++ jobfinder.client/src/App.vue
@@ -44,44 +44,44 @@ components: {
       sources: [
         {
           name:'linkedIn',
-          img:'./img/linkedin-logo-large.png'
+          img:'linkedin-logo-large.png'
         },
         {
           name:'rabotaBy',
-          img:'./img/rabotaby-logo-large.png'
+          img:'rabotaby-logo-large.png'
         },
         {
           name:'devBy',
-          img:'./img/devby-logo-large.png'
+          img:'devby-logo-large.png'
         },
         {
           name:'pracaBy',
-          img:'./img/pracaby-logo-large.png'
+          img:'pracaby-logo-large.png'
         }
       ],
 
       jobs: [],
       urls: [
         {
-          img: '.img/linkedin-logo-small.png',
+          img: 'linkedin-logo-small.png',
           name: 'linkedIn',
           path: 'https://www.linkedin.com/search/results/all/?',
           active: false
         },
         {
-          img: '.img/rabotaby-logo-small.png',
+          img: 'rabotaby-logo-small.png',
           name: 'rabotaBy',
           path: 'https://rabota.by/search/vacancy/?',
           active: false
         },
         {
-          img: '.img/devby-logo-small.png',
+          img: 'devby-logo-small.png',
           name: 'devBy',
           path: 'https://jobs.devby.io/?',
           active: false
         },
         {
-          img: '.img/pracaby-logo-small.png',
+          img: 'pracaby-logo-small.png',
           name: 'pracaBy',
           path: 'https://praca.by/search/vacancies/?',
           active: false
@@ -105,7 +105,7 @@ components: {
           this.loading = true;
 
           loadedJobs = await fetch(
-            'https://localhost:49606/Jobs/GetList',
+            'https://localhost:7150/Jobs/GetList',
             {
               body: JSON.stringify({
                 url: url.path,
