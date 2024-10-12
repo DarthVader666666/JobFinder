122a53fb99b8aac1d2446bcaf82b9bd52fb4ef24
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-10-12 11:48:07 +0300

    Commented SSL certificate check

--- jobfinder.client/vite.config.js
+++ jobfinder.client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-vue';
 import fs from 'fs';
 import path from 'path';
-import child_process from 'child_process';
+//import child_process from 'child_process';
 import { env } from 'process';
 
 const baseFolder =
@@ -16,19 +16,19 @@ const certificateName = "jobfinder.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error(`Could not create certificate. ${certFilePath} ${keyFilePath}`);
-    }
-}
+//if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//    if (0 !== child_process.spawnSync('dotnet', [
+//        'dev-certs',
+//        'https',
+//        '--export-path',
+//        certFilePath,
+//        '--format',
+//        'Pem',
+//        '--no-password',
+//    ], { stdio: 'inherit', }).status) {
+//        throw new Error(`Could not create certificate. ${certFilePath} ${keyFilePath}`);
+//    }
+//}
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7150';
