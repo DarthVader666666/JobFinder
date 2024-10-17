c7273137f9a2be27ecd7a95de1c68af711e08fd9
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-17 22:11:29 +0300

    Rebuilt to weatherReport

--- JobFinder.Server/Program.cs
+++ JobFinder.Server/Program.cs
@@ -1,28 +1,22 @@
-using Microsoft.AspNetCore.Cors.Infrastructure;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
-builder.Services.AddRouting();
-builder.Services.AddCors(options => options.AddPolicy("AllowAll",
-    new CorsPolicyBuilder().AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().Build()));
-
-builder.Services.AddLogging(builder => builder.AddConsole());
-
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-    options.AllowSynchronousIO = true;
-});
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
+app.UseStaticFiles();
 
-app.UseRouting();
-app.UseCors();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
 
 app.Run();
