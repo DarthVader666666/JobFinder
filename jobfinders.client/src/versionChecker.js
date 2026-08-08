6b1a8a693ebf7a21f39e4efb30fc2abeb76f07ef
DarthVader666666 <rumyancer@gmail.com>
2026-08-08 20:12:53 +0300

    Removed conslole.log

--- jobfinders.client/src/versionChecker.js
+++ jobfinders.client/src/versionChecker.js
@@ -3,19 +3,15 @@ import { ref } from "vue";
 const currentVersion = ref(null);
 
 async function loadVersion() {
-  console.log("Before Fetch");
   const res = await fetch("version.json", { cache: "no-store" });
   const json = await res.json();
-  console.log("Fetch Result: " + json);
   return json.version;
 }
 
 async function startChecking(onUpdateDetected) {
   currentVersion.value = await loadVersion();
-  console.log("Current Version: " + currentVersion.value);
   setInterval(async () => {
     const serverVersion = await loadVersion();
-    console.log("Server Version:" + serverVersion);
     if (serverVersion !== currentVersion.value) {
       onUpdateDetected();
     }
