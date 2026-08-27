adfb5a988a3e2122701f36a53c324158ba1b0346
DarthVader666666 <rumyancer@gmail.com>
2026-08-27 22:15:27 +0300

    Configured JWT

---
+++ JobFinders.Api/Models/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace JobFinders.Api.Models
+{
+    public class LoginRequest
+    {
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
